6bf880bed1631f31494ceb2edcc294f3e9813d27
comfreestyle <vr_freestyle@outlook.com>
2015-08-13 23:00:22 +0700

    Fixed bug GameActivity

--- app/src/main/java/com/example/kotchaphanmuangsan/hangmangame/GameActivity.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/hangmangame/GameActivity.java
@@ -2,14 +2,26 @@ package com.example.kotchaphanmuangsan.hangmangame;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 /**
  * Created by Kotchaphan Muangsan on 13/8/2558.
  */
-public class GameActivity extends Activity{
+public class GameActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+    }
+
+    public void introdueLetter(View v) {
+        EditText editText = (EditText) findViewById(R.id.editTextLetter);
+
+        //get edittext values
+        String letter = editText.getText().toString();
+        Log.d("Log EditText" , "Letter is : "+letter);
+
     }
 }
