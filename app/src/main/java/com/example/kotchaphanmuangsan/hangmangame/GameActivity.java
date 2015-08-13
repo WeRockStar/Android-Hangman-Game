5629fcdb38d80aac3484fef83fc4720bdc4b391f
comfreestyle <vr_freestyle@outlook.com>
2015-08-13 23:59:16 +0700

    Then word limit let clear and Win

--- app/src/main/java/com/example/kotchaphanmuangsan/hangmangame/GameActivity.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/hangmangame/GameActivity.java
@@ -10,6 +10,8 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 /**
  * Created by Kotchaphan Muangsan on 13/8/2558.
  */
@@ -17,6 +19,7 @@ public class GameActivity extends Activity {
 
     String mWord = "WORD";
     int mFailedCounter = 0;
+    int mGuessedLetters = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +31,8 @@ public class GameActivity extends Activity {
         EditText editText = (EditText) findViewById(R.id.editTextLetter);
         //get edittext values
         String letter = editText.getText().toString();
+        editText.setText("");
+
         Log.d("Log EditText", "Letter is : " + letter);
         if (letter.length() > 0 && letter.length() == 1) {
             checkLetter(letter);
@@ -46,19 +51,69 @@ public class GameActivity extends Activity {
                 Log.d("Word", "There was one match is : " + charFromTheWord);
                 letterGuessed = true;
                 showLetterAtIndex(i, charIntroduce);
+
+                mGuessedLetters++;
             }
         }
 
         if (letterGuessed == false) {
-            letterFailed();
+            letterFailed(Character.toString(charIntroduce));
+        }
+
+        if (mGuessedLetters == mWord.length()) {
+            //TODO Winer Score One Point
+            //TODO Clear
+            clearScreen();
+
         }
     }
 
-    public void letterFailed() {
+    //TODO Clear
+    public void clearScreen() {
+        TextView textView = (TextView) findViewById(R.id.textView6);
+        textView.setText("");
+
+        mGuessedLetters = 0;
+        mFailedCounter = 0;
+
+        LinearLayout layoutLetter = (LinearLayout) findViewById(R.id.layoutLetters);
+        for (int i = 0; i < layoutLetter.getChildCount(); i++) {
+            TextView currentTextView = (TextView) layoutLetter.getChildAt(i);
+            currentTextView.setText("_");
+        }
+
+        //clear image to zero 
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.hangdroid_0);
+    }
+
+    public void letterFailed(String letterFail) {
+
+        //Respone Show Text
+        TextView textView = (TextView) findViewById(R.id.textView6);
+
+        String previousFail = textView.getText().toString();
+
+        textView.setText(previousFail + letterFail);
+
         //mfailedCounter++
         mFailedCounter = mFailedCounter + 1;
-
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        //change image
+        if (mFailedCounter == 1) {
+            imageView.setImageResource(R.drawable.hangdroid_1);
+        } else if (mFailedCounter == 2) {
+            imageView.setImageResource(R.drawable.hangdroid_2);
+        } else if (mFailedCounter == 3) {
+            imageView.setImageResource(R.drawable.hangdroid_3);
+        } else if (mFailedCounter == 4) {
+            imageView.setImageResource(R.drawable.hangdroid_4);
+        } else if (mFailedCounter == 5) {
+            imageView.setImageResource(R.drawable.hangdroid_5);
+        } else if (mFailedCounter == 6) {
+            //game over
+        }
     }
 
     public void showLetterAtIndex(int position, char letterGuessed) {
