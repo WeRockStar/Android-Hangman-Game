f2080a0cb725a9482f7c40e228afecb60c392369
comfreestyle <vr_freestyle@outlook.com>
2015-08-13 22:09:07 +0700

    Design Game Screen

--- app/src/main/java/com/example/kotchaphanmuangsan/hangmangame/GameActivity.java
+++ app/src/main/java/com/example/kotchaphanmuangsan/hangmangame/GameActivity.java
@@ -1,10 +1,15 @@
 package com.example.kotchaphanmuangsan.hangmangame;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Kotchaphan Muangsan on 13/8/2558.
  */
 public class GameActivity extends Activity{
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+    }
 }
